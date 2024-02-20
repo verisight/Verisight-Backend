25b9fbcc618a25a9abee979b3cc25f4ff7183299
Mohamed Aashif <maashif011@gmail.com>
2024-02-21 02:12:24 +0530

    Refactor CrossCheck method signature in CrosscheckController

--- src/crosscheck/crosscheck.controller.ts
+++ src/crosscheck/crosscheck.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CrosscheckService } from './crosscheck.service';
+import { CreateArticleDto } from 'src/articles/dto/create-article.dto';
 
 @Controller('crosscheck')
 export class CrosscheckController {
     constructor(private readonly crosscheckService: CrosscheckService) {}
 
     @Post()
-    async CrossCheck(@Body() article: {headline: string, body: string}) {
+    async CrossCheck(@Body() article: CreateArticleDto) {
         const result = await this.crosscheckService.getCrosscheck(article);
         return result;
     }
