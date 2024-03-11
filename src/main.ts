9212d2280ffa9799195f2c15ae12ea0f698de088
ishmaifan <ishma07777@gmail.com>
2024-03-11 14:32:03 +0530

    add CRO

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as passport from "passport";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.use(
     session({
       secret:"verisight",
