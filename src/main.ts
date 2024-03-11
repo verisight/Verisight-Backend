76df957cb5bd07f485362714e1a0dfdc9035cae3
ishmaifan <ishma07777@gmail.com>
2024-03-11 20:10:21 +0530

    add cookies config

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as passport from "passport";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({origin: 'http://localhost:5173',credentials: true});
   app.use(
     session({
       secret:"verisight",
