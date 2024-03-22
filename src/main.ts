f4d9567fbd2ccb0134011046a03769adf6c5db0d
PPT1001 <pragash1001@gmail.com>
2024-03-22 23:50:36 +0530

    Added comments

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import * as passport from 'passport';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
+  // Create the Nest application instance and set up the express application
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  // Enabling CORS for the application to allow requests from the client
   app.enableCors({
     origin: [
       'http://localhost:5173',
@@ -15,22 +17,25 @@ async function bootstrap() {
     credentials: true,
   });
   app.set('trust proxy', 1);
+  // Enable sessions for the application to store user data in the session
   app.use(
     session({
       secret: 'verisight',
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: true,
+        secure: false,
         domain: 'verisightlabs.com',
         maxAge: 1000 * 60 * 60 * 24 * 365,
         httpOnly: false,
       },
     }),
   );
+  // Enable passport middleware for authentication
   app.use(passport.initialize());
   app.use(passport.session());
 
+  // Start the application on the specified port
   await app.listen(process.env.PORT || 3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
   console.log(`Application is running on port ${process.env.PORT}`);
